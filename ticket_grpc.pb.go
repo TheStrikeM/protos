c54944d9ca5e5296dae7c45c93bb51b61f0f6f3f
Thestrikem <thestrikem@gmail.com>
2024-01-11 18:34:38 +0300

    test

--- ticket_grpc.pb.go
+++ ticket_grpc.pb.go
@@ -19,194 +19,194 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// ComplainClient is the client API for Complain service.
+// TicketServiceClient is the client API for TicketService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type ComplainClient interface {
+type TicketServiceClient interface {
 	AllTickets(ctx context.Context, in *AllTicketsRequest, opts ...grpc.CallOption) (*AllTicketsResponse, error)
 	CreateTicket(ctx context.Context, in *CreateTicketRequest, opts ...grpc.CallOption) (*CreateTicketResponse, error)
 	ResolveTicket(ctx context.Context, in *ResolveTicketRequest, opts ...grpc.CallOption) (*empty.Empty, error)
 	TicketByTrader(ctx context.Context, in *TicketByTraderRequest, opts ...grpc.CallOption) (*TicketByTraderResponse, error)
 }
 
-type complainClient struct {
+type ticketServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewComplainClient(cc grpc.ClientConnInterface) ComplainClient {
-	return &complainClient{cc}
+func NewTicketServiceClient(cc grpc.ClientConnInterface) TicketServiceClient {
+	return &ticketServiceClient{cc}
 }
 
-func (c *complainClient) AllTickets(ctx context.Context, in *AllTicketsRequest, opts ...grpc.CallOption) (*AllTicketsResponse, error) {
+func (c *ticketServiceClient) AllTickets(ctx context.Context, in *AllTicketsRequest, opts ...grpc.CallOption) (*AllTicketsResponse, error) {
 	out := new(AllTicketsResponse)
-	err := c.cc.Invoke(ctx, "/ticket_proto.Complain/AllTickets", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/ticket_proto.TicketService/AllTickets", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *complainClient) CreateTicket(ctx context.Context, in *CreateTicketRequest, opts ...grpc.CallOption) (*CreateTicketResponse, error) {
+func (c *ticketServiceClient) CreateTicket(ctx context.Context, in *CreateTicketRequest, opts ...grpc.CallOption) (*CreateTicketResponse, error) {
 	out := new(CreateTicketResponse)
-	err := c.cc.Invoke(ctx, "/ticket_proto.Complain/CreateTicket", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/ticket_proto.TicketService/CreateTicket", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *complainClient) ResolveTicket(ctx context.Context, in *ResolveTicketRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+func (c *ticketServiceClient) ResolveTicket(ctx context.Context, in *ResolveTicketRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
 	out := new(empty.Empty)
-	err := c.cc.Invoke(ctx, "/ticket_proto.Complain/ResolveTicket", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/ticket_proto.TicketService/ResolveTicket", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *complainClient) TicketByTrader(ctx context.Context, in *TicketByTraderRequest, opts ...grpc.CallOption) (*TicketByTraderResponse, error) {
+func (c *ticketServiceClient) TicketByTrader(ctx context.Context, in *TicketByTraderRequest, opts ...grpc.CallOption) (*TicketByTraderResponse, error) {
 	out := new(TicketByTraderResponse)
-	err := c.cc.Invoke(ctx, "/ticket_proto.Complain/TicketByTrader", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/ticket_proto.TicketService/TicketByTrader", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// ComplainServer is the server API for Complain service.
-// All implementations must embed UnimplementedComplainServer
+// TicketServiceServer is the server API for TicketService service.
+// All implementations must embed UnimplementedTicketServiceServer
 // for forward compatibility
-type ComplainServer interface {
+type TicketServiceServer interface {
 	AllTickets(context.Context, *AllTicketsRequest) (*AllTicketsResponse, error)
 	CreateTicket(context.Context, *CreateTicketRequest) (*CreateTicketResponse, error)
 	ResolveTicket(context.Context, *ResolveTicketRequest) (*empty.Empty, error)
 	TicketByTrader(context.Context, *TicketByTraderRequest) (*TicketByTraderResponse, error)
-	mustEmbedUnimplementedComplainServer()
+	mustEmbedUnimplementedTicketServiceServer()
 }
 
-// UnimplementedComplainServer must be embedded to have forward compatible implementations.
-type UnimplementedComplainServer struct {
+// UnimplementedTicketServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedTicketServiceServer struct {
 }
 
-func (UnimplementedComplainServer) AllTickets(context.Context, *AllTicketsRequest) (*AllTicketsResponse, error) {
+func (UnimplementedTicketServiceServer) AllTickets(context.Context, *AllTicketsRequest) (*AllTicketsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AllTickets not implemented")
 }
-func (UnimplementedComplainServer) CreateTicket(context.Context, *CreateTicketRequest) (*CreateTicketResponse, error) {
+func (UnimplementedTicketServiceServer) CreateTicket(context.Context, *CreateTicketRequest) (*CreateTicketResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateTicket not implemented")
 }
-func (UnimplementedComplainServer) ResolveTicket(context.Context, *ResolveTicketRequest) (*empty.Empty, error) {
+func (UnimplementedTicketServiceServer) ResolveTicket(context.Context, *ResolveTicketRequest) (*empty.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ResolveTicket not implemented")
 }
-func (UnimplementedComplainServer) TicketByTrader(context.Context, *TicketByTraderRequest) (*TicketByTraderResponse, error) {
+func (UnimplementedTicketServiceServer) TicketByTrader(context.Context, *TicketByTraderRequest) (*TicketByTraderResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method TicketByTrader not implemented")
 }
-func (UnimplementedComplainServer) mustEmbedUnimplementedComplainServer() {}
+func (UnimplementedTicketServiceServer) mustEmbedUnimplementedTicketServiceServer() {}
 
-// UnsafeComplainServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to ComplainServer will
+// UnsafeTicketServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to TicketServiceServer will
 // result in compilation errors.
-type UnsafeComplainServer interface {
-	mustEmbedUnimplementedComplainServer()
+type UnsafeTicketServiceServer interface {
+	mustEmbedUnimplementedTicketServiceServer()
 }
 
-func RegisterComplainServer(s grpc.ServiceRegistrar, srv ComplainServer) {
-	s.RegisterService(&Complain_ServiceDesc, srv)
+func RegisterTicketServiceServer(s grpc.ServiceRegistrar, srv TicketServiceServer) {
+	s.RegisterService(&TicketService_ServiceDesc, srv)
 }
 
-func _Complain_AllTickets_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _TicketService_AllTickets_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(AllTicketsRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ComplainServer).AllTickets(ctx, in)
+		return srv.(TicketServiceServer).AllTickets(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/ticket_proto.Complain/AllTickets",
+		FullMethod: "/ticket_proto.TicketService/AllTickets",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ComplainServer).AllTickets(ctx, req.(*AllTicketsRequest))
+		return srv.(TicketServiceServer).AllTickets(ctx, req.(*AllTicketsRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Complain_CreateTicket_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _TicketService_CreateTicket_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateTicketRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ComplainServer).CreateTicket(ctx, in)
+		return srv.(TicketServiceServer).CreateTicket(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/ticket_proto.Complain/CreateTicket",
+		FullMethod: "/ticket_proto.TicketService/CreateTicket",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ComplainServer).CreateTicket(ctx, req.(*CreateTicketRequest))
+		return srv.(TicketServiceServer).CreateTicket(ctx, req.(*CreateTicketRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Complain_ResolveTicket_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _TicketService_ResolveTicket_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ResolveTicketRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ComplainServer).ResolveTicket(ctx, in)
+		return srv.(TicketServiceServer).ResolveTicket(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/ticket_proto.Complain/ResolveTicket",
+		FullMethod: "/ticket_proto.TicketService/ResolveTicket",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ComplainServer).ResolveTicket(ctx, req.(*ResolveTicketRequest))
+		return srv.(TicketServiceServer).ResolveTicket(ctx, req.(*ResolveTicketRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Complain_TicketByTrader_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _TicketService_TicketByTrader_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(TicketByTraderRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ComplainServer).TicketByTrader(ctx, in)
+		return srv.(TicketServiceServer).TicketByTrader(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/ticket_proto.Complain/TicketByTrader",
+		FullMethod: "/ticket_proto.TicketService/TicketByTrader",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ComplainServer).TicketByTrader(ctx, req.(*TicketByTraderRequest))
+		return srv.(TicketServiceServer).TicketByTrader(ctx, req.(*TicketByTraderRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// Complain_ServiceDesc is the grpc.ServiceDesc for Complain service.
+// TicketService_ServiceDesc is the grpc.ServiceDesc for TicketService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Complain_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "ticket_proto.Complain",
-	HandlerType: (*ComplainServer)(nil),
+var TicketService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "ticket_proto.TicketService",
+	HandlerType: (*TicketServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "AllTickets",
-			Handler:    _Complain_AllTickets_Handler,
+			Handler:    _TicketService_AllTickets_Handler,
 		},
 		{
 			MethodName: "CreateTicket",
-			Handler:    _Complain_CreateTicket_Handler,
+			Handler:    _TicketService_CreateTicket_Handler,
 		},
 		{
 			MethodName: "ResolveTicket",
-			Handler:    _Complain_ResolveTicket_Handler,
+			Handler:    _TicketService_ResolveTicket_Handler,
 		},
 		{
 			MethodName: "TicketByTrader",
-			Handler:    _Complain_TicketByTrader_Handler,
+			Handler:    _TicketService_TicketByTrader_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
